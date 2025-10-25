34a2c0f675301a381defcec5fa5ea6ca373fb6ab
nkvesp <nkvesper@gmail.com>
2025-10-25 15:56:46 +0300

    Завершено основний дизайн

--- assets/js/mainpage.js
+++ assets/js/mainpage.js
@@ -1,2 +1,13 @@
-import { addContent } from "./loadcontent.js";
-console.log(addContent)
+// import { addContent } from "./loadcontent.js";
+// console.log(addContent)
+const quizDataUrl = "./assets/data/data.json"
+
+async function addContent() {
+    const response = await fetch(quizDataUrl)
+    let quizData = await response.json()
+    console.log(quizData);
+
+    return quizData
+}
+const data = addContent()
+console.log(data.quizzes);
